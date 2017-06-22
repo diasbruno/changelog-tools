92c38e39e1967b8cb7539d1ccbc8d5c230faceac
Bruno Dias <dias.h.bruno@gmail.com>
2017-06-22 02:11:27 -0300

    wip: adding hg utils.

--- changelogtools/git/__init__.py
+++ changelogtools/git/__init__.py
@@ -1,14 +1,8 @@
-from subprocess import Popen, PIPE
-
-
-def git_exec(args):
-    p = Popen(" ".join(["git"] + args), shell=True, stdout=PIPE, stderr=PIPE)
-    out, err = p.communicate()
-    return out.decode('utf-8')
+from .. import scm_exec
 
 
 def git_log(args):
-    return git_exec(["log"] + args)
+    return scm_exec('git', ["log"] + args)
 
 
 def git_logs_between_versions(a, b):
